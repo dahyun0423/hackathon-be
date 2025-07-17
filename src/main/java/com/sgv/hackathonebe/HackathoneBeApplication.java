df69c2bbfeaa44fb218341ff40a4cdbad75ad3be
dahyun0423 <ockda0423@naver.com>
2025-07-17 14:35:31 +0900

    :gear: Setting

---
+++ src/main/java/com/sgv/hackathonebe/HackathoneBeApplication.java
@@ -0,0 +1,13 @@
+package com.sgv.hackathonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathoneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathoneBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sgv/hackathonebe/HackathoneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgv.hackathonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathoneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
